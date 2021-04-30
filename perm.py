785ae3625f6be307977de5b8001c73989f54a7d2
andymurh <andymurh@gmail.com>
2021-04-30 17:58:16 +0200

    simplify logic a bit

--- perm.py
+++ perm.py
@@ -4,10 +4,7 @@ def perm_step(accum, l, r):
 
     if len(r) <= 1:
         return
-
-    if not accum:
-        accum.append(l + r)
-
+        
     for ind in range(len(r)):
         r_copy = list(r)
         r_copy[0], r_copy[ind] = r_copy[ind], r_copy[0]
@@ -18,13 +15,13 @@ def perm_step(accum, l, r):
 # returns list with all possible permutations (as a lists) with n elements
 # e.g. [[1,2], [2,1]] if n = 2
 def perm(n):
-    res = []
     init = list(range(1, n + 1))
+    res = [init]
     perm_step(res, [], init)
     return res
 
 if __name__ == "__main__":
-    n = 4
+    n = 3
     print("permutations for n: {0}".format(n))
     res = perm(n)
     print(res)
